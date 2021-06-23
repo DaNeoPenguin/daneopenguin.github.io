425ff95f56178c3c39b90b56f50e6cc0b553ac45
titoudelaplage <totolegamerofficiel@gmail.com>
2021-06-23 12:15:56 +0200

    messageacommitsurmain

--- linuxsh/mcserver1.17.sh
+++
@@ -1,9 +0,0 @@
-mkdir MinecraftServer
-apt-get install -y wget apt-transport-https gnupg
-apt-get update
-cd MinecraftServer
-wget https://download.getbukkit.org/spigot/spigot-1.17.jar
-
-wget https://titoudelaplage.github.io/linuxsh/minecraftserver.files/start.sh
-
-echo "you need to start the server by typing start.sh  in the termnial"
--- linuxsh/minecraftserver.files/start.sh
+++
@@ -1,2 +0,0 @@
-java -Xms512M -Xmx1024M -jar Spigot-1.17.jar 
-pause
